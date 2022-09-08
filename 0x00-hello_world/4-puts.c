3200040a2168436ed824262c76fa2a0c319efbc8
Adeniyi Adedapo <dapatinto1@gmail.com>
2022-09-08 15:09:35 -0700

    Committing Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  *
  *  * main - Entry point
@@ -8,8 +9,13 @@
  *    * Return: Always 0 (Success)
  *
  *     */
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
+
 }
