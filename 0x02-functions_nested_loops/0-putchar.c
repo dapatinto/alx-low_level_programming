5ced9a587f28b25013555596c872842b68e3c511
Adeniyi Adedapo <dapatinto1@gmail.com>
2022-09-13 14:02:48 -0700

    Committing putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  *
  *  *main - Program that prints _putchar
@@ -12,30 +10,16 @@
  *
  *     */
 
-
-
 int main(void)
-
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 
-		_putchar('_');
-
-			_putchar('p');
-
-				_putchar('u');
-
-					_putchar('t');
-
-						_putchar('c');
-
-							_putchar('h');
-
-								_putchar('a');
-
-									_putchar('r');
-
-										_putchar('\n');
-
-											return (0);
-
-}
