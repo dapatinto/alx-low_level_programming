761812244d971c26dabe863c6c84802646b63202
Adeniyi Adedapo <dapatinto1@gmail.com>
2022-09-15 15:47:35 -0700

    Committing I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,12 +6,12 @@
  *
  */
 
-	void print_diagonal(int n)
+void print_diagonal(int n)
 {
-	int i, j;
+int i, j;
 	if (n <= 0)
 {
-	_putchar('\n');
+_putchar('\n');
 }
 	else
 {
@@ -19,10 +19,10 @@
 {
 	for (j = 0; j < i; j++)
 {
-	_putchar(' ');
+_putchar(' ');
 }
-	_putchar('\\');
-	_putchar('\n');
+_putchar('\\');
+_putchar('\n');
 }
 }
 }
