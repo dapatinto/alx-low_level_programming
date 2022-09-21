76f0a3958d0be4399818ea1a9866ea67341dc9a2
Adeniyi Adedapo <dapatinto1@gmail.com>
2022-09-21 12:19:45 -0700

    A dream does not become reality through magic, it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -18,17 +18,17 @@ int main(void)
 
 		    /*
 		     *
-		     *    * write your line of code here...
+		     * * write your line of code here...
 		     *
-		     *       * Remember:
+		     * * Remember:
 		     *
-		     *          * - you are not allowed to use a
+		     * * - you are not allowed to use a
 		     *
-		     *             * - you are not allowed to modify p
+		     * * - you are not allowed to modify p
 		     *
-		     *                * - only one statement
+		     * * - only one statement
 		     *
-		     *                   * - you are not allowed to code anything else than this line of code
+		     * * - you are not allowed to code anything else than this line of code
 		     *
 		     *                      */
 
@@ -36,8 +36,8 @@ int main(void)
 
 		      /* ...so that this prints 98\n */
 
-		      printf("a[2] = %d\n", a[2]);
+		 printf("a[2] = %d\n", a[2]);
 
-		        return (0);
-
+		 return (0);
 }
+
